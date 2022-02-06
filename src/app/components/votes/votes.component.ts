957a43bf43d6e57264a6e4ecbf4acdd4fcd07343
amani-joseph <mr.amanijoseph@gmail.com>
2022-02-06 21:52:27 +0300

    Enhanced styling for visual appeal

--- src/app/components/votes/votes.component.ts
+++ src/app/components/votes/votes.component.ts
@@ -7,14 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotesComponent implements OnInit {
   Votes: number = 0;
+  upVotes: number = 0;
+  downVotes: number = 0;
 
   upVote() {
     console.log('upVote');
     this.Votes = this.Votes += 1;
+    this.upVotes = this.upVotes += 1;
   }
   downVote() {
     console.log('downVote');
-    this.Votes = this.Votes -= 1;
+    this.Votes = this.Votes += 1;
+    this.downVotes = this.downVotes += 1;
   }
 
   constructor() {}
