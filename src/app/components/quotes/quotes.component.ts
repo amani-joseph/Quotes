0a5e2319af2af9c1f642d035b05222a5a6f367be
amani-joseph <mr.amanijoseph@gmail.com>
2022-02-06 14:03:54 +0300

    upvote and Downvote buttons are now functional but with a bug. Need to make sure that the vote counter updates with every click

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -8,39 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class QuotesComponent implements OnInit {
   InputQuote: string = '';
   InputAuthor: string = '';
-  upVotes: number = 0;
-  downVotes: number = 0;
+  Votes: number = 0;
+  upVote() {
+    console.log('upVote');
+    this.Votes++;
+  }
+  downVote() {
+    console.log('downVote');
+    this.Votes--;
+  }
   quotes = [
     {
       id: 1,
-      upVotes: 0,
-      downVotes: 0,
+      Votes: 0,
       quote: 'The purpose of our lives is to be happy.',
       author: 'Joe',
     },
-    {
-      id: 2,
-      upVotes: 0,
-      downVotes: 0,
-      quote: 'Life is what happens when you’re busy making other plans.',
-      author: 'Jack',
-    },
-    {
-      id: 3,
-      upVotes: 0,
-      downVotes: 0,
-      quote: 'Get busy living or get busy dying',
-      author: 'Jim',
-    },
+    // {
+    //   id: 2,
+    //   upVotes: 0,
+    //   downVotes: 0,
+    //   quote: 'Life is what happens when you’re busy making other plans.',
+    //   author: 'Jack',
+    // },
+    // {
+    //   id: 3,
+    //   upVotes: 0,
+    //   downVotes: 0,
+    //   quote: 'Get busy living or get busy dying',
+    //   author: 'Jim',
+    // },
   ];
-  upVote(upVotes: number): any {
-    let newUpVotes = upVotes + 1;
-    return newUpVotes;
-    console.log(newUpVotes);
-  }
-  downVote(upVotes: number): void {
-    upVotes -= 1;
-  }
 
   removeQuote(id: number): void {
     this.quotes = this.quotes.filter((quote) => quote.id !== id);
@@ -56,8 +54,7 @@ export class QuotesComponent implements OnInit {
     const uniqueId: number = Math.trunc(Math.random() * 1000);
     const newQuote = {
       id: uniqueId,
-      upVotes: 0,
-      downVotes: 0,
+      Votes: 0,
       quote: this.InputQuote,
       author: this.InputAuthor,
     };
